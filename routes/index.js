79662b7da2c3cf713d46d662d1f036c0dfd470ac
Rafid Hoda <rafidhoda@gmail.com>
2017-12-27 20:41:48 +0100

    Contact form validation

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,28 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Bitcamp Platform' });
 });
 
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', { title: 'Bitcamp Platform' });
+  })
+  .post(function(req, res, next) {
+    req.checkBody('name', 'Empty name').notEmpty();
+    req.checkBody('email', 'Invalid email').isEmail();
+    req.checkBody('message', 'Empty message').notEmpty();
+    var errors = req.validationErrors();
+
+    if(errors) {
+      res.render('contact', {
+        title: 'Bitcamp Platform',
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        errorMessages: errors
+      });
+    } else {
+      res.render('thank', { title: 'Bitcamp Platform' });
+    }
+  });
+
+
 module.exports = router;
