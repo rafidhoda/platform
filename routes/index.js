8897835c0079d4e1ed5dcc1838f3fdd99cb36c40
Rafid Hoda <rafidhoda@gmail.com>
2017-12-27 20:04:41 +0100

    Add about page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bitcamp Platform' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Bitcamp Platform' });
 });
 
 module.exports = router;
